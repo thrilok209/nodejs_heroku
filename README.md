f7e62411b4809f598fb96984d7e328c32d69f0ba
thrilok209 <thrilok2000@gmail.com>
2018-08-24 04:15:32 +0530

    update to key

---
+++ README.md
@@ -0,0 +1,3 @@
+An backend code for heroku.
+
+It monitors the Skyminer code and sends alert through email.
---
+++ index.js
@@ -0,0 +1,140 @@
+const request = require('request');
+const express = require('express')
+const app = express();
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+var allNode = 0;
+var presentNodes = [];
+var presentOnlineNode=[];
+var  presentOnlineNodeIndes=[]
+var presentOfflineNode=[]
+var presentOfflineNodeINdex=[]
+var  showofflinenodes=[]
+const sendmail = require('sendmail')();
+var nodemailer = require('nodemailer');
+var sendingmail = true;
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.ID,
+    pass: process.env.PASS
+  }
+});
+
+var mailOptions = {
+  from: process.env.ID,
+  to: process.env.EID,
+  subject: 'Skyminer Nodes check',
+  text: `OFFLINE NODE: ${presentOfflineNodeINdex}`
+};
+
+setInterval(api, 5000);
+setInterval(sendMail,1000*30)
+// setImmediate(sendMail)
+
+function sendMail() {
+  if(presentOfflineNodeINdex.length!=0 && sendingmail == true){
+    transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+  }
+
+}
+
+
+const miner01 = ["032a7513c051c4d2384afca093f86e65888e87645ac0b05b72ca212508b335f8c6",
+  "025e9f499126d3af4193c26311fcc75571a18580bb323d89e8113bff45ec14b849",
+  "02b19bc5c05f253b6c46b76c09a2553c22c60728f8839640f5d85baa5fc5fa7d2e",
+  "03c8f798346a532a37ddc8c50c70f0328e4754c27a2e6b3cd0b61f3b05504e84b0",
+  "021ec73e251529dec08d4074c4f99778aff7f0e032300e6e2a911ee7c53737d839",
+  "02a3daa62ac22e803190293d19a7394ac13d56ca8db88dd53aa8c1c8bd9bffbba3",
+  "037a74c678f8209dc1ade5c865789cac9300398ca5088f7d494d1e4fe834a492fe",
+  "025e7ff53fac57eb584ac4c57aef8ec171fc6437adba78dbd1ae32ba07185d65bd"];
+
+emitter.on('data', (arg)=> {
+  allNode = []
+  allNode = arg;
+  // console.log(allNode);
+
+checkOnline();
+
+
+
+});
+
+
+
+function api() {
+  request('http://discovery.skycoin.net:8001/conn/getAll', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    // console.log(err);
+    emitter.emit('data', body );
+
+  });
+}
+
+
+function checkOnline()
+{
+presentOnlineNode=[]
+presentOnlineNodeIndes=[]
+presentOfflineNodeINdex=[]
+presentOfflineNode=[]
+
+for(let i=0;i<allNode.length;i++){
+for(let j=0;j<miner01.length;j++){
+  if(miner01[j]==allNode[i].key){
+    presentOnlineNode.push(miner01[j])
+
+  }
+}
+}
+
+for(let i=0;i<miner01.length;i++){
+var index = presentOnlineNode.indexOf(miner01[i]);
+
+if(index == (-1)) {
+
+presentOfflineNode.push(miner01[i]);
+presentOfflineNodeINdex.push(i+1);
+
+}
+if(index != (-1)) {
+
+// presentOfflineNode.push(miner01[i]);
+presentOnlineNodeIndes.push(i+1);
+
+}
+
+}
+// console.log(presentOfflineNodeINdex)
+// console.log("OK");
+console.log(sendingmail);
+
+
+
+}
+
+app.get('/online', function (req, res) {
+  res.send(presentOfflineNodeINdex)
+})
+app.get('/offline', function (req, res) {
+  res.send(presentOnlineNodeIndes)
+})
+app.get('/start', function (req, res) {
+  res.send('Turned ON mailing alert!!!')
+  sendingmail = true;
+})
+app.get('/stop', function (req, res) {
+  res.send('Turned OFF mailing alert!!!')
+  sendingmail = false;
+
+})
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on port ${port}`));
